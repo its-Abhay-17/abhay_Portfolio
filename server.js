7ac0cb0274f13063b0229e86416ccd6b0c746aed
Abhay <abhayfbd890@gmail.com>
2026-09-17 23:08:31 +0530

    Make files and body section with some basic styling

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express()
 const port = 3000
 const path = require('path')
+const ejsMate = require('ejs-mate');
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname,'public/')))
 
